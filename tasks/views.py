7cdbf6bd7e78f36e2704be612311982f4a544beb
kolen <incredible.angst@gmail.com>
2014-09-26 18:34:37 +0400

    Reindent

--- tasks/views.py
+++ tasks/views.py
@@ -6,26 +6,26 @@ import json
 
 
 def home(request):
-	return render_to_response("index.html")
+    return render_to_response("index.html")
 
 
 @require_http_methods(['GET', 'HEAD', 'POST'])
 def tasks(request):
-	if request.method in ['GET', 'HEAD']:
-		dump = json.dumps([
-			{'id': t.id, 'description': t.description, 'parent': t.parent_id}
-			 for t in Task.objects.all()
-			 ])
-		return HttpResponse(dump, content_type="application/json")
-	elif request.method in ['POST']:
-		data = json.loads(request.body)
-		task = Task()
-		task.description = data['description']
-		task.parent_id = data['parent']
-		task.save()
-		return HttpResponse('')
+    if request.method in ['GET', 'HEAD']:
+        dump = json.dumps([
+            {'id': t.id, 'description': t.description, 'parent': t.parent_id}
+             for t in Task.objects.all()
+             ])
+        return HttpResponse(dump, content_type="application/json")
+    elif request.method in ['POST']:
+        data = json.loads(request.body)
+        task = Task()
+        task.description = data['description']
+        task.parent_id = data['parent']
+        task.save()
+        return HttpResponse('')
 
 
 @require_http_methods(['PUT'])
 def task_put(request, id):
-	pass
+    pass
