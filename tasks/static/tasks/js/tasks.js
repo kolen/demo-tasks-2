cf99055dee0530291e11123dbdfbf2c1278a90ed
kolen <incredible.angst@gmail.com>
2014-09-26 17:06:18 +0400

    Treeify function

--- tasks/static/tasks/js/tasks.js
+++ tasks/static/tasks/js/tasks.js
@@ -67,4 +67,26 @@ angular.module('tasks', ['ui.sortable', 'RecursionHelper'])
         return RecursionHelper.compile(element);
       }
     };
-}]);
+}])
+.factory('treeify', function() {
+  return function treeify(list, idAttr, parentAttr, childrenAttr) {
+    if (!idAttr) idAttr = 'id';
+    if (!parentAttr) parentAttr = 'parent';
+    if (!childrenAttr) childrenAttr = 'children';
+
+    var treeList = [];
+    var lookup = {};
+    list.forEach(function(obj) {
+        lookup[obj[idAttr]] = obj;
+        obj[childrenAttr] = [];
+    });
+    list.forEach(function(obj) {
+        if (obj[parentAttr] != null) {
+            lookup[obj[parentAttr]][childrenAttr].push(obj);
+        } else {
+            treeList.push(obj);
+        }
+    });
+    return treeList;
+  };
+});
