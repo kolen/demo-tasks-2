7169abc6a64a9c10b6c7118c230ba5c06b4f19c5
kolen <incredible.angst@gmail.com>
2014-09-26 23:25:57 +0400

    Protect on delete on foreign key (don't allow to delete whole subtree)

--- tasks/models.py
+++ tasks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Task(models.Model):
 	description = models.TextField()
 	weight = models.IntegerField(null=True, blank=True)
-	parent = models.ForeignKey('Task', null=True, blank=True)
+	parent = models.ForeignKey('Task', null=True, blank=True, on_delete=models.PROTECT)
 
 	def __unicode__(self):
 		return "<Task %s %s %s>" % (self.pk, self.description, self.parent)
